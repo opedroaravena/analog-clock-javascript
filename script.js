bf85e140a97ee1059e541ab52dd31ca21c98a800
opedroaravena <pedroaravena.contato@gmail.com>
2021-07-22 23:25:52 -0300

    finishing javascript

--- script.js
+++ script.js
@@ -2,16 +2,39 @@
 //SELECTING ELEMENTS THAT I WILL USE TO MANIPULATE
 let digitalElement = document.querySelector('.digital')
 let sElement = document.querySelector('.p_s')
-let mElement = document.querySelector('.p_s')
-let hElement = document.querySelector('.p_s')
+let mElement = document.querySelector('.p_m')
+let hElement = document.querySelector('.p_h')
 
 function updateClock(){
     let now = new Date()
-    let hour = now.getHours
-    let minute = now.getMinutes
-    let second = now.getSeconds
+    let hour = now.getHours()
+    let minute = now.getMinutes()
+    let second = now.getSeconds() 
 
-    digitalElement.innerHTML = `${hour}:${minute}:${second}:`
+    //Injecting info to the digital class [line 35 index.html]
+
+    //Digital clock
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+
+    //analog clock
+    //calculando o circulo do relogio 360 / 60 = 6 graus
+    let sDeg = ((360 / 60) * second) - 90
+    let mDeg = ((360 / 60) * minute) - 90
+    let hDeg = ((360 / 12) * hour) - 90
+
+    //colocando propriedado do CSS no JS
+    sElement.style.transform = `rotate(${sDeg}deg)`
+    mElement.style.transform = `rotate(${mDeg}deg)`
+    hElement.style.transform = `rotate(${hDeg}deg)`
+}
+
+function fixZero(time){
+    // if(time < 10){
+    //     return '0'+ time
+    // } else {
+    //     return time
+    // }
+    return time < 10 ? `0${time}`: time
 }
 
 setInterval(updateClock, 1000)
